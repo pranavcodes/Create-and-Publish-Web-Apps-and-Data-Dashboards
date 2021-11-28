bcf3dbe52ec6ced861fe9da5a7e26acc411aee36
Pranav Mishra <pranav4code@gmail.com>
2021-11-28 08:29:54 +0000

    Added a table

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
+data = {'Series 1': [1, 3, 4, 5, 7],
+        'Series 2': [10, 30, 40, 100, 250]}
+
+df  = pd.DataFrame(data)
 st.title("Our First Streamlit App")
 st.subheader("Introducing Strimlit in Automate Everything With Python")
 st.write("This is our first web app. Enjoy!!")
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
