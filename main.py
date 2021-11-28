66c632fc332e55720dd11013d763eb780b9f82f6
Professor <>
2021-11-28 08:18:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Our First Streamlit App")
+st.subheader("Introducing Strimlit in Automate Everything With Python")
+st.write("This is our first web app. Enjoy!!")
